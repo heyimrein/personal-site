3fe5bbc476eeeefa52e6d90bc275e9c6627b1b75
reinlmao <rekreingd@gmail.com>
2023-07-03 08:52:21 -0500

    Reformat animation math

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -10,45 +10,55 @@ import { useRef, useState } from "react";
 
 export default function CardSection() {
   const target = useRef(null);
-  const [initialTransition, setInitialTransition] = useState(0.75);
-  const [finalTransition, setFinalTransition] = useState(0);
+  const [initialOpacity, setInitialOpacity] = useState(0.75);
+  const [finalOpacity, setFinalOpacity] = useState(0);
+
   const { scrollYProgress } = useScroll({
     target,
   });
-  const initialTransitionMotion = useTransform(
-    scrollYProgress,
-    [0, 0.2],
-    [0.75, 1]
-  );
-  const finalTransitionMotion = useTransform(
-    scrollYProgress,
-    [0.8, 1],
-    [0, 0.25]
-  );
-  const cardScrollProgress = useTransform(
+  const cardScrollOffset = useTransform(
     scrollYProgress,
     [0, 1],
     ["120vh", "-85vh"]
   );
 
-  useMotionValueEvent(initialTransitionMotion, "change", (val) => {
-    setInitialTransition(val);
+  const initialTransitionMotion = useTransform(
+    scrollYProgress,
+    [0, 0.2],
+    [0, 1]
+  );
+  const finalTransitionMotion = useTransform(scrollYProgress, [0.8, 1], [0, 1]);
+
+  const opacityDelta = 0.25;
+  const initialOpacityMotion = useTransform(
+    initialTransitionMotion,
+    [0, 1],
+    [0, opacityDelta]
+  );
+  const finalOpacityMotion = useTransform(
+    finalTransitionMotion,
+    [0, 1],
+    [0, opacityDelta]
+  );
+
+  useMotionValueEvent(initialOpacityMotion, "change", (val) => {
+    setInitialOpacity(val);
   });
-  useMotionValueEvent(finalTransitionMotion, "change", (val) => {
-    setFinalTransition(val);
+  useMotionValueEvent(finalOpacityMotion, "change", (val) => {
+    setFinalOpacity(val);
   });
 
   return (
     <motion.div
       className="card-section"
       ref={target}
-      style={{ scale: initialTransition - finalTransition }}
+      style={{
+        opacity: 1 - opacityDelta + (initialOpacity - finalOpacity),
+        scale: 1 - opacityDelta + (initialOpacity - finalOpacity),
+      }}
     >
       <div className="card-backdrop">
-        <motion.div
-          className="card-container"
-          style={{ y: cardScrollProgress }}
-        >
+        <motion.div className="card-container" style={{ y: cardScrollOffset }}>
           <DescriptionCard>He/him, 20 Years Old; Freelancer</DescriptionCard>
           <DescriptionCard>5+ years of Software Dev Experience</DescriptionCard>
           <DescriptionCard>
