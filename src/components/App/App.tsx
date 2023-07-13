8c206b421f441f88f1ef8df27f8289827204591c
reinlmao <rekreingd@gmail.com>
2023-07-13 15:54:27 -0500

    Remove unused import

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import IntroSection from "../sections/IntroSection/IntroSection.tsx";
 import ChatSection from "../sections/ChatSection/ChatSection.tsx";
-import CardSection from "../sections/CardSection/CardSection.tsx";
 
 export default function App() {
   return (
