df7cde45ecc70f98a9ac65985bd19b5f1592f70d
reinlmao <rekreingd@gmail.com>
2023-07-15 16:38:36 -0500

    Polish CardSection

---
+++ src/components/ScrollCard/ScrollCard.tsx
@@ -0,0 +1,37 @@
+import { motion, Reorder } from "framer-motion";
+import "./ScrollCard.css";
+
+export default function ScrollCard(props: {
+  leftContent: string;
+  rightContent: string;
+}) {
+  const cardVariants = {
+    whileHover: {
+      y: "-0.25em",
+      transition: {
+        duration: 0.25,
+        stiffness: 200,
+        bounce: 1000,
+      },
+    },
+  };
+
+  return (
+    <div className="scroll-card">
+      <motion.div
+        className="scroll-card-left"
+        variants={cardVariants}
+        whileHover="whileHover"
+      >
+        <span>{props.leftContent}</span>
+      </motion.div>
+      <motion.div
+        className="scroll-card-right"
+        variants={cardVariants}
+        whileHover="whileHover"
+      >
+        <span>{props.rightContent}</span>
+      </motion.div>
+    </div>
+  );
+}
--- src/components/sections/CardSection/CardSection.tsx
+++ src/components/sections/CardSection/CardSection.tsx
@@ -1,10 +1,24 @@
 import "./CardSection.css";
-import PullCard from "../../PullCard/PullCard.tsx";
+import ScrollCard from "../../ScrollCard/ScrollCard.tsx";
+import { useState } from "react";
+
+export type Item = {
+  key: number;
+  leftContent: string;
+  rightContent: string;
+};
 
 export default function CardSection() {
+  const [items, setItems] = useState<Item[]>([
+    { key: 0, leftContent: "Lorem Ipsum", rightContent: "Lorem Ipsum" },
+    { key: 1, leftContent: "Lorem Ipsum", rightContent: "Lorem Ipsum" },
+  ]);
+
   return (
-    <div className="card-section">
-      <PullCard>Lorem Ipsum</PullCard>
+    <div id="card-section">
+      <ScrollCard leftContent="Lorem Ipsum" rightContent="Lorem Ipsum" />
+      <ScrollCard leftContent="Lorem Ipsum" rightContent="Lorem Ipsum" />
+      <ScrollCard leftContent="Lorem Ipsum" rightContent="Lorem Ipsum" />
     </div>
   );
 }
