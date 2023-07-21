15de6a97782ed70b224d8345d9c0d2137fe06dda
rein <rekreingd@gmail.com>
2023-07-21 14:58:46 -0500

    Add AboutMeSection

---
+++ src/components/sections/AboutMeSection/AboutMeSection.tsx
@@ -0,0 +1,11 @@
+import "./AboutMeSection.css";
+
+export default function AboutMeSection() {
+  return <div id="about-me-section">
+    <header className="about-me-header">About Me</header>
+    <span className="about-me-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+    <div className="about-me-divider" />
+    <header className="about-me-header">Other About Me</header>
+    <span className="about-me-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
+  </div>
+}
